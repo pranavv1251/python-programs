5e8002cd75ed507414d2a3d2884ed79a8268e94b
pranavv1251 <pranavvgangurde1251@gmail.com>
2020-03-18 12:15:03 +0530

    modified credentials

--- sms.py
+++ sms.py
@@ -1,13 +1,13 @@
 from twilio.rest import Client
 
-account_sid = 'AC568cdbe940e071258c338a38057b892e'
-auth_token = '3c60c41998a3410e6a8f9d079f49fde7'
+account_sid = '<Account SID>'
+auth_token = '<Authorization Token>'
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
     from_='+12077394223',
     to='+919689558750',
-    body='HThis is a system message!'
+    body='This is a system message!'
 )
 
 print(message.sid)
