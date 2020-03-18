f1e70d035432c95b2eb80f657dc866b81fa711fa
pranavv1251 <pranavvgangurde1251@gmail.com>
2020-03-18 10:49:33 +0530

    modified message

--- sms.py
+++ sms.py
@@ -1,14 +1,13 @@
-# Phone Number:-  +12077394223
 from twilio.rest import Client
 
 account_sid = 'AC568cdbe940e071258c338a38057b892e'
-auth_token = '7e93cf42e513c830e279099b4cf79455'
+auth_token = '3c60c41998a3410e6a8f9d079f49fde7'
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
     from_='+12077394223',
-    body='Happy Birthday!',
-    to='+919689558750'
+    to='+919689558750',
+    body='HThis is a system message!'
 )
 
 print(message.sid)
